76a714dfef8e3a47adfc3dccc860c6d5f27e2bd8
Jay Garcia <jesusdgarcia@gmail.com>
2019-07-22 13:17:26 -0400

    Re-organized files

--- src/Resources.r
+++ src/Resources.r
@@ -1,16 +1,28 @@
-PATH resources
+PATH resources/charset
 BITMAP charset_8x8.bmp
 BITMAP charset_16x16.bmp
 
+PATH resources/
 BITMAP splash.bmp
 BITMAP splash_sprites.bmp
 BITMAP title.bmp
 
-PATH resources/32x32/character
+PATH resources/sprites/
 BITMAP chara_hero.bmp
 
-PATH resources/32x32/environment
+#PATH src/resources/sprites/enemies
+#BITMAP chara_bat.bmp
+#BITMAP chara_goblin.bmp
+#BITMAP chara_goblin_sniper.bmp
+#BITMAP chara_orc.bmp
+#BITMAP chara_rat.bmp
+#BITMAP chara_slime.bmp
+#BITMAP chara_spider.bmp
+#BITMAP chara_troll.bmp
+#BITMAP projectile_arrow.bmp
 
+
+PATH resources/tilemaps/example_dungeon
 BITMAP Dungeon1.tiles.bmp
 RAW jays_example.tlc
 RAW Dungeon1.jays_example.layer-Layer 1.map001.stm
--- src/resources/tilemaps/Dungeon_Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Dungeon_Tileset" tilewidth="16" tileheight="16" tilecount="480" columns="20">
- <image source="../sprites/environment/Dungeon_Tileset.bmp" trans="ff00ff" width="320" height="384"/>
-</tileset>
