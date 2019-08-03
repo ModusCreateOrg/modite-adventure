e7c5d505c21513303ba7d9e8e7c67f35174aef60
Michael Tintiuc <michaeltintiuc@users.noreply.github.com>
2019-08-04 01:12:09 +0300

    Fixes #23 (#24)

--- src/GameState/GGameState.cpp
+++ src/GameState/GGameState.cpp
@@ -3,7 +3,7 @@
 #include "GPlayerProcess.h"
 
 GGameState::GGameState() : BGameEngine(gViewPort) {
-  gViewPort->SetRect(TRect(0,0, 10*32, 7*32));
+  gViewPort->SetRect(TRect(0, 0, MIN(SCREEN_WIDTH, 10 * 32) - 1, MIN(SCREEN_HEIGHT, 7 * 32) - 1));
   gResourceManager.LoadBitmap(CHARA_HERO_BMP, PLAYER_SLOT, IMAGE_64x64);
   mPlayfield = mGamePlayfield = new GGamePlayfield(gViewPort, EXAMPLE_FILELIST_TXT_MAP);
   mPlayerProcess = new GPlayerProcess(this, mGamePlayfield);
@@ -15,6 +15,6 @@ GGameState::~GGameState() {
 }
 
 void GGameState::PreRender() {
-//  gDisplay.renderBitmap->Clear();
+ gDisplay.renderBitmap->Clear();
 }
 
