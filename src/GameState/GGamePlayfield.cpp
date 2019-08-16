5612b4fce1d902863e4cdf7cfbd898777226c1dc
Michael Schwartz <mykesx@gmail.com>
2019-08-16 09:20:33 -0700

    fix hero colors

--- src/GameState/GGamePlayfield.cpp
+++ src/GameState/GGamePlayfield.cpp
@@ -3,7 +3,7 @@
 GGamePlayfield::GGamePlayfield(BViewPort *aViewPort, TUint16 aTileMapId) : BMapPlayfield(aViewPort, aTileMapId) {
   gDisplay.SetPalette(this->mTileset, 0, 128);
   auto bm = gResourceManager.GetBitmap(PLAYER_SLOT);
-  gDisplay.SetPalette(bm->GetPalette(), PLAYER_PALETTE, SPIDER_COLORS);
+  gDisplay.SetPalette(bm->GetPalette(), PLAYER_PALETTE, PLAYER_COLORS);
   bm = gResourceManager.GetBitmap(SPIDER_SLOT);
   gDisplay.SetPalette(bm->GetPalette(), SPIDER_PALETTE, SPIDER_COLORS);
 }
