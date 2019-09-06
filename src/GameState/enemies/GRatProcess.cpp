c20c922f40654d5bc6fccc3d5e192f7ada0871be
Michael Schwartz <mykesx@gmail.com>
2019-09-06 07:10:43 -0700

    wip

--- src/GameState/enemies/GRatProcess.cpp
+++ src/GameState/enemies/GRatProcess.cpp
@@ -19,6 +19,8 @@ const TInt DEATH_SPEED = 5;
 
 const TFloat VELOCITY = 1.5;
 
+// region ANIMATIONS {{{
+
 /*********************************************************************************
  *********************************************************************************
  *********************************************************************************/
@@ -35,27 +37,16 @@ const TFloat VELOCITY = 1.5;
 |___\__,_|_|\___/_/  |____/ \___|_|\___|\___|\__\___|\__,_|
  */
 
-ANIMSCRIPT idleAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
-  ASTEP(40, IMG_RAT_IDLE),
-  ASTEP(4, IMG_RAT_IDLE + 1),
-  ASTEP(40, IMG_RAT_IDLE + 2),
-  ASTEP(4, IMG_RAT_IDLE + 1),
-  ALOOP
-};
+ANIMSCRIPT idleAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
+  ASTEP(40, IMG_RAT_IDLE), ASTEP(4, IMG_RAT_IDLE + 1),
+  ASTEP(40, IMG_RAT_IDLE + 2), ASTEP(4, IMG_RAT_IDLE + 1), ALOOP };
 
-static ANIMSCRIPT selectAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
+static ANIMSCRIPT selectAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
   ASTEP(SELECT_SPEED, IMG_RAT_SELECTED + 0),
   ASTEP(SELECT_SPEED, IMG_RAT_SELECTED + 1),
-  ASTEP(SELECT_SPEED, IMG_RAT_SELECTED + 2),
-  ALOOP
-};
+  ASTEP(SELECT_SPEED, IMG_RAT_SELECTED + 2), ALOOP };
 
-static ANIMSCRIPT deathAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT deathAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(DEATH_SPEED, IMG_RAT_WALK_RIGHT + 0),
   ASTEP(DEATH_SPEED, IMG_RAT_WALK_UP + 0),
   AFLIP(DEATH_SPEED, IMG_RAT_WALK_RIGHT + 0),
@@ -63,9 +54,7 @@ static ANIMSCRIPT deathAnimation[] = {
   ASTEP(DEATH_SPEED, IMG_RAT_WALK_RIGHT + 0),
   ASTEP(DEATH_SPEED, IMG_RAT_WALK_UP + 0),
   AFLIP(DEATH_SPEED, IMG_RAT_WALK_RIGHT + 0),
-  ASTEP(DEATH_SPEED, IMG_RAT_WALK_DOWN + 0),
-  AEND
-};
+  ASTEP(DEATH_SPEED, IMG_RAT_WALK_DOWN + 0), AEND };
 
 /*
  ____
@@ -75,49 +64,30 @@ static ANIMSCRIPT deathAnimation[] = {
 |____/ \___/ \_/\_/ |_| |_|
 */
 
-static ANIMSCRIPT idleDownAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2),
-  ALOOP
-};
+static ANIMSCRIPT idleDownAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0), ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2), ALOOP };
 
-static ANIMSCRIPT walkDownAnimation1[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkDownAnimation1[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 0),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 1),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 1), AEND };
 
-static ANIMSCRIPT walkDownAnimation2[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkDownAnimation2[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 2),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 3),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_DOWN + 3), AEND };
 
-static ANIMSCRIPT attackDownAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT attackDownAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 3),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 0),
-  ATYPE(STYPE_EBULLET),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 1),
-  ATYPE(STYPE_ENEMY),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 2),
-  AEND
-};
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 0), ATYPE(STYPE_EBULLET),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 1), ATYPE(STYPE_ENEMY),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_DOWN + 2), AEND };
 
-static ANIMSCRIPT hitDownAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT hitDownAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 3),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 0),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 1),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 2),
-  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 3),
-  AEND
-};
+  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_DOWN + 3), AEND };
 
 /*
  _          __ _
@@ -128,49 +98,30 @@ static ANIMSCRIPT hitDownAnimation[] = {
 
  */
 
-static ANIMSCRIPT idleLeftAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2),
-  ALOOP
-};
+static ANIMSCRIPT idleLeftAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0), ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2), ALOOP };
 
-static ANIMSCRIPT walkLeftAnimation1[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkLeftAnimation1[] = { ABITMAP(RAT_SLOT),
   AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 0),
-  AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 1),
-  AEND
-};
+  AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 1), AEND };
 
-static ANIMSCRIPT walkLeftAnimation2[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkLeftAnimation2[] = { ABITMAP(RAT_SLOT),
   AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 2),
-  AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 3),
-  AEND
-};
+  AFLIP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 3), AEND };
 
-static ANIMSCRIPT attackLeftAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT attackLeftAnimation[] = { ABITMAP(RAT_SLOT),
   AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 3),
-  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 0),
-  ATYPE(STYPE_EBULLET),
-  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 1),
-  ATYPE(STYPE_ENEMY),
-  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 2),
-  AEND
-};
+  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 0), ATYPE(STYPE_EBULLET),
+  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 1), ATYPE(STYPE_ENEMY),
+  AFLIP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 2), AEND };
 
-static ANIMSCRIPT hitLeftAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT hitLeftAnimation[] = { ABITMAP(RAT_SLOT),
   AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3),
   AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 0),
   AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 1),
   AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 2),
-  AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3),
-  AEND
-};
+  AFLIP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3), AEND };
 
 /*
  ____  _       _     _
@@ -181,49 +132,30 @@ static ANIMSCRIPT hitLeftAnimation[] = {
          |___/
  */
 
-static ANIMSCRIPT idleRightAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2),
-  ALOOP
-};
+static ANIMSCRIPT idleRightAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0), ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2), ALOOP };
 
-static ANIMSCRIPT walkRightAnimation1[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkRightAnimation1[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 0),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 1),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 1), AEND };
 
-static ANIMSCRIPT walkRightAnimation2[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkRightAnimation2[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 2),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 3),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_RIGHT + 3), AEND };
 
-static ANIMSCRIPT attackRightAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT attackRightAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 3),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 0),
-  ATYPE(STYPE_EBULLET),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 1),
-  ATYPE(STYPE_ENEMY),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 2),
-  AEND
-};
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 0), ATYPE(STYPE_EBULLET),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 1), ATYPE(STYPE_ENEMY),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_RIGHT + 2), AEND };
 
-static ANIMSCRIPT hitRightAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT hitRightAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 0),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 1),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 2),
-  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3),
-  AEND
-};
+  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_RIGHT + 3), AEND };
 
 /*
  _   _
@@ -234,58 +166,41 @@ static ANIMSCRIPT hitRightAnimation[] = {
       |_|
  */
 
-static ANIMSCRIPT idleUpAnimation[] = {
-  ABITMAP(RAT_SLOT),
-  ALABEL,
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
-  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2),
-  ALOOP
-};
+static ANIMSCRIPT idleUpAnimation[] = { ABITMAP(RAT_SLOT), ALABEL,
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 0), ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 1),
+  ASTEP(IDLE_SPEED, IMG_RAT_IDLE + 2), ALOOP };
 
-
-static ANIMSCRIPT walkUpAnimation1[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkUpAnimation1[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 0),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 1),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 1), AEND };
 
-static ANIMSCRIPT walkUpAnimation2[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT walkUpAnimation2[] = { ABITMAP(RAT_SLOT),
   ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 2),
-  ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 3),
-  AEND
-};
+  ASTEP(WALK_SPEED, IMG_RAT_WALK_UP + 3), AEND };
 
-static ANIMSCRIPT attackUpAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT attackUpAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 3),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 0),
-  ATYPE(STYPE_EBULLET),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 1),
-  ATYPE(STYPE_ENEMY),
-  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 2),
-  AEND
-};
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 0), ATYPE(STYPE_EBULLET),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 1), ATYPE(STYPE_ENEMY),
+  ASTEP(ATTACK_SPEED, IMG_RAT_ATTACK_UP + 2), AEND };
 
-static ANIMSCRIPT hitUpAnimation[] = {
-  ABITMAP(RAT_SLOT),
+static ANIMSCRIPT hitUpAnimation[] = { ABITMAP(RAT_SLOT),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 3),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 0),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 1),
   ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 2),
-  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 3),
-  AEND
-};
+  ASTEP(HIT_SPEED, IMG_RAT_DAMAGE_UP + 3), AEND };
+
+// endregion }}}
 
 /*********************************************************************************
  *********************************************************************************
  *********************************************************************************/
 
 // constructor
-GRatProcess::GRatProcess(GGameState *aGameState, GGamePlayfield *aGamePlayfield, TFloat aX, TFloat aY)
-  : GEnemyProcess(aGameState, aGamePlayfield, RAT_SLOT ) {
+GRatProcess::GRatProcess(GGameState *aGameState, GGamePlayfield *aGamePlayfield,
+    TFloat aX, TFloat aY)
+    : GEnemyProcess(aGameState, aGamePlayfield, RAT_SLOT) {
   mSprite->x = aX;
   mSprite->y = aY;
   mSprite->mHitPoints = HIT_POINTS;
@@ -303,7 +218,8 @@ GRatProcess::~GRatProcess() {
 
 void GRatProcess::NewState(TUint16 aState, DIRECTION aDirection) {
 #ifdef DEBUGME
-  printf("RAT NewState(%d,%d) %s[mState]\n", aState, aDirection, stateMessages[aState]);
+  printf("RAT NewState(%d,%d) %s[mState]\n", aState, aDirection,
+      stateMessages[aState]);
 #endif
   mState = aState;
   mSprite->mDirection = aDirection;
@@ -346,7 +262,8 @@ void GRatProcess::NewState(TUint16 aState, DIRECTION aDirection) {
 #endif
           mStep = 1 - mStep;
           mSprite->vy = VELOCITY;
-          mSprite->StartAnimation(mStep ? walkDownAnimation1 : walkDownAnimation2);
+          mSprite->StartAnimation(
+              mStep ? walkDownAnimation1 : walkDownAnimation2);
           break;
         case DIRECTION_LEFT:
 #ifdef DEBUGME
@@ -354,8 +271,9 @@ void GRatProcess::NewState(TUint16 aState, DIRECTION aDirection) {
 #endif
           mStep = 1 - mStep;
           mSprite->vx = -VELOCITY;
-//          mSprite->mDx = -36;
-          mSprite->StartAnimation(mStep ? walkLeftAnimation1 : walkLeftAnimation2);
+          //          mSprite->mDx = -36;
+          mSprite->StartAnimation(
+              mStep ? walkLeftAnimation1 : walkLeftAnimation2);
           break;
         case DIRECTION_RIGHT:
 #ifdef DEBUGME
@@ -363,7 +281,8 @@ void GRatProcess::NewState(TUint16 aState, DIRECTION aDirection) {
 #endif
           mStep = 1 - mStep;
           mSprite->vx = VELOCITY;
-          mSprite->StartAnimation(mStep ? walkRightAnimation1 : walkRightAnimation2);
+          mSprite->StartAnimation(
+              mStep ? walkRightAnimation1 : walkRightAnimation2);
           break;
       }
       break;
@@ -455,10 +374,9 @@ TBool GRatProcess::IdleState() {
   }
   if (--mStateTimer < 0) {
     // Set distance to walk for WALK_STATE
-    mStateTimer = TInt16(TFloat(Random(1,3)) * 32 / VELOCITY);
+    mStateTimer = TInt16(TFloat(Random(1, 3)) * 32 / VELOCITY);
 
-    TFloat x = mSprite->x,
-           y = mSprite->y,
+    TFloat x = mSprite->x, y = mSprite->y,
            sx = x - mGameState->GetViewPort()->mWorldX,
            sy = y - mGameState->GetViewPort()->mWorldY;
 
@@ -471,25 +389,31 @@ TBool GRatProcess::IdleState() {
 
       switch (direction) {
         case 0: // up
-          if (sy > 16 && !mPlayfield->IsWall(x + 16, y - 32 - VELOCITY) && !mPlayfield->IsWall(x + 48, y - 32 - VELOCITY)) {
+          if (sy > 16 && !mPlayfield->IsWall(x + 16, y - 32 - VELOCITY) &&
+              !mPlayfield->IsWall(x + 48, y - 32 - VELOCITY)) {
             NewState(WALK_STATE, DIRECTION_UP);
             return ETrue;
           }
           break;
         case 1: // down
-          if (sy < (SCREEN_HEIGHT-16) && !mPlayfield->IsWall(x + 16, y + VELOCITY) && !mPlayfield->IsWall(x + 48, y + VELOCITY)) {
+          if (sy < (SCREEN_HEIGHT - 16) &&
+              !mPlayfield->IsWall(x + 16, y + VELOCITY) &&
+              !mPlayfield->IsWall(x + 48, y + VELOCITY)) {
             NewState(WALK_STATE, DIRECTION_DOWN);
             return ETrue;
           }
           break;
         case 2: // left
-          if (sx > 16 && !mPlayfield->IsWall(x + 16 - VELOCITY, y + 32) && !mPlayfield->IsWall(x + 16 - VELOCITY, y)) {
+          if (sx > 16 && !mPlayfield->IsWall(x + 16 - VELOCITY, y + 32) &&
+              !mPlayfield->IsWall(x + 16 - VELOCITY, y)) {
             NewState(WALK_STATE, DIRECTION_LEFT);
             return ETrue;
           }
           break;
         case 3: // right
-          if (sx < (SCREEN_WIDTH-16) && !mPlayfield->IsWall(x + 48 + VELOCITY, y + 32) && !mPlayfield->IsWall(x + 48 + VELOCITY, y)) {
+          if (sx < (SCREEN_WIDTH - 16) &&
+              !mPlayfield->IsWall(x + 48 + VELOCITY, y + 32) &&
+              !mPlayfield->IsWall(x + 48 + VELOCITY, y)) {
             NewState(WALK_STATE, DIRECTION_RIGHT);
             return ETrue;
           }
@@ -516,20 +440,25 @@ TBool GRatProcess::WalkState() {
   }
 
   BViewPort *vp = mGameState->GetViewPort();
-  TFloat screenX = mSprite->x - vp->mWorldX,
-            screenY = mSprite->y - vp->mWorldY;
+  TFloat screenX = mSprite->x - vp->mWorldX, screenY = mSprite->y - vp->mWorldY;
 
 #ifdef DEBUGME
-  printf("RAT screenX, screenY = %f,%f, x,y = %f,%f\n", screenX, screenY, mSprite->x, mSprite->y);
+  printf("RAT screenX, screenY = %f,%f, x,y = %f,%f\n", screenX, screenY,
+      mSprite->x, mSprite->y);
 #endif
 
   if (--mStateTimer < 0 ||
-                      mPlayfield->IsWall(mSprite->x + 16 + mSprite->vx, mSprite->y + mSprite->vy) ||      // Left/Bottom Wall
-                      mPlayfield->IsWall(mSprite->x + 16 + mSprite->vx, mSprite->y - 32 + mSprite->vy) || // Left/Top Wall
-                      mPlayfield->IsWall(mSprite->x + 48 + mSprite->vx, mSprite->y + mSprite->vy) ||      // Right/Bottom Wall
-                      mPlayfield->IsWall(mSprite->x + 48 + mSprite->vx, mSprite->y - 32 + mSprite->vy) || // Right/Top Wall
-                      screenX < 16 || screenX > (SCREEN_WIDTH - 16) || screenY < 16 || screenY > (SCREEN_HEIGHT - 16)
-    ) {
+      mPlayfield->IsWall(mSprite->x + 16 + mSprite->vx,
+          mSprite->y + mSprite->vy) || // Left/Bottom Wall
+      mPlayfield->IsWall(mSprite->x + 16 + mSprite->vx,
+          mSprite->y - 32 + mSprite->vy) || // Left/Top Wall
+      mPlayfield->IsWall(mSprite->x + 48 + mSprite->vx,
+          mSprite->y + mSprite->vy) || // Right/Bottom Wall
+      mPlayfield->IsWall(mSprite->x + 48 + mSprite->vx,
+          mSprite->y - 32 + mSprite->vy) || // Right/Top Wall
+      screenX < 16 ||
+      screenX > (SCREEN_WIDTH - 16) || screenY < 16 ||
+      screenY > (SCREEN_HEIGHT - 16)) {
     NewState(IDLE_STATE, mSprite->mDirection);
     return ETrue;
   }
@@ -588,7 +517,4 @@ TBool GRatProcess::RunBefore() {
   }
 }
 
-TBool GRatProcess::RunAfter() {
-  return ETrue;
-}
-
+TBool GRatProcess::RunAfter() { return ETrue; }
--- src/GameState/player/GPlayerProcess.cpp
+++ src/GameState/player/GPlayerProcess.cpp
@@ -3,23 +3,23 @@
 #include "GGamePlayfield.h"
 #include "GPlayerAnimations.h"
 
-const TUint16 IDLE_STATE = 0;
-const TUint16 WALK_STATE = 1;
+const TUint16 IDLE_STATE  = 0;
+const TUint16 WALK_STATE  = 1;
 const TUint16 SWORD_STATE = 2;
-const TUint16 FALL_STATE = 3;
+const TUint16 FALL_STATE  = 3;
 
 GPlayerProcess::GPlayerProcess(GGameState *aGameState) {
   mGameState = aGameState;
   mPlayfield = ENull;
-  mSprite = new GAnchorSprite(1, PLAYER_SLOT);
-  mSprite->type = STYPE_PLAYER;
+  mSprite    = new GAnchorSprite(1, PLAYER_SLOT);
+  mSprite->type  = STYPE_PLAYER;
   mSprite->cMask = STYPE_ENEMY;
-  mSprite->x = mSprite->y = 32;
-  mSprite->w = 32;
-  mSprite->h = 32;
+  mSprite->x     = mSprite->y = 32;
+  mSprite->w     = 32;
+  mSprite->h     = 32;
   mGameState->AddSprite(mSprite);
   mState = IDLE_STATE;
-  mStep = 0;
+  mStep  = 0;
   mSprite->flags |= SFLAG_ANCHOR | SFLAG_SORTY | SFLAG_CHECK;
   NewState(IDLE_STATE, DIRECTION_DOWN);
 }
@@ -29,7 +29,8 @@ GPlayerProcess::~GPlayerProcess() {
   delete mSprite;
 }
 
-void GPlayerProcess::StartLevel(GGamePlayfield *aPlayfield, TFloat aX, TFloat aY) {
+void GPlayerProcess::StartLevel(
+  GGamePlayfield *aPlayfield, TFloat aX, TFloat aY) {
   mPlayfield = aPlayfield;
   mSprite->x = aX;
   mSprite->y = aY;
@@ -47,19 +48,33 @@ TBool GPlayerProcess::IsLedge(TFloat aX, TFloat aY) {
   return mPlayfield->GetAttribute(aX, aY) == ATTR_LEDGE;
 }
 
+TBool GPlayerProcess::IsLedge(TRect &aRect) {
+  return IsLedge(aRect.x1, aRect.y2) || IsLedge(aRect.x2, aRect.y2);
+}
+
+TBool GPlayerProcess::IsLedge() {
+  return (IsLedge(mSprite->x + mSprite->cx + mSprite->w / 2, mSprite->y + 4));
+}
+
 TBool GPlayerProcess::CanWalk(TRect &aRect) {
   if (aRect.x1 < 0 || aRect.y1 < 0) {
     return EFalse;
   }
-  return IsFloor(aRect.x1, aRect.y2) || IsLedge(aRect.x1, aRect.y2) ||
-         IsFloor(aRect.x2, aRect.y2) || IsLedge(aRect.x2, aRect.y2);
+  if(IsFloor(aRect.x1, aRect.y2) || IsFloor(aRect.x2, aRect.y2)) {
+    return ETrue;
+  }
+  if ( IsLedge(aRect)) {
+    const TInt y = mSprite->y;
+    mSprite->y = y | 3;
+    return ETrue;
+  }
 }
 
 void GPlayerProcess::NewState(TUint16 aState, DIRECTION aDirection) {
   mState = aState;
   mSprite->mDirection = aDirection;
-  mSprite->mDx = 0;
-  mSprite->mDy = 0;
+  mSprite->mDx        = 0;
+  mSprite->mDy        = 0;
   switch (mState) {
     case IDLE_STATE:
       mStep = 0;
@@ -92,18 +107,21 @@ void GPlayerProcess::NewState(TUint16 aState, DIRECTION aDirection) {
         case DIRECTION_DOWN:
           mStep = 1 - mStep;
           mSprite->vy = VELOCITY;
-          mSprite->StartAnimation(mStep ? walkDownAnimation1 : walkDownAnimation2);
+          mSprite->StartAnimation(
+            mStep ? walkDownAnimation1 : walkDownAnimation2);
           break;
         case DIRECTION_LEFT:
           mStep = 1 - mStep;
           mSprite->vx = -VELOCITY;
-//          mSprite->mDx = -36;
-          mSprite->StartAnimation(mStep ? walkLeftAnimation1 : walkLeftAnimation2);
+          //          mSprite->mDx = -36;
+          mSprite->StartAnimation(
+            mStep ? walkLeftAnimation1 : walkLeftAnimation2);
           break;
         case DIRECTION_RIGHT:
           mStep = 1 - mStep;
           mSprite->vx = VELOCITY;
-          mSprite->StartAnimation(mStep ? walkRightAnimation1 : walkRightAnimation2);
+          mSprite->StartAnimation(
+            mStep ? walkRightAnimation1 : walkRightAnimation2);
           break;
       }
       break;
@@ -160,7 +178,9 @@ TBool GPlayerProcess::MaybeSword() {
 }
 
 TBool GPlayerProcess::MaybeFall() {
-  if (IsLedge(mSprite->x+mSprite->cx+mSprite->w/2, mSprite->y)) {
+  if (IsLedge()) {
+    printf("IsLedge");
+//  if (IsLedge(mSprite->mRect)) {
     NewState(FALL_STATE, DIRECTION_DOWN);
     return ETrue;
   }
@@ -172,28 +192,28 @@ TBool GPlayerProcess::MaybeWalk() {
   if (MaybeSword()) {
     return ETrue;
   }
-  
-  const TFloat x = mSprite->x,
-      y = mSprite->y;
-  TRect r;
-  
+
+  const TFloat x = mSprite->x, y = mSprite->y;
+  TRect        r;
+
   if (gControls.IsPressed(JOYLEFT)) {
     r.Set(x - VELOCITY, y - 18, x + 22 - VELOCITY, y);
     if (!CanWalk(r)) {
-//      mPlayfield->IsWall(mSprite->x + 22 - VELOCITY, mSprite->y) ||
-//          mPlayfield->IsWall(mSprite->x + 22 - VELOCITY, mSprite->y - 18)) {
+      //      mPlayfield->IsWall(mSprite->x + 22 - VELOCITY, mSprite->y) ||
+      //          mPlayfield->IsWall(mSprite->x + 22 - VELOCITY, mSprite->y -
+      //          18)) {
       return EFalse;
     }
-    
+
     if (mState != WALK_STATE || mSprite->mDirection != DIRECTION_LEFT) {
       NewState(WALK_STATE, DIRECTION_LEFT);
     }
     return ETrue;
   }
-  
+
   if (gControls.IsPressed(JOYRIGHT)) {
     r.Set(x + 42 + VELOCITY, y, x + 42 + VELOCITY, y - 18);
-    
+
     if (!CanWalk(r)) {
       return EFalse;
     }
@@ -202,40 +222,41 @@ TBool GPlayerProcess::MaybeWalk() {
     }
     return ETrue;
   }
-  
+
   if (gControls.IsPressed(JOYUP)) {
-    r.Set(x + 22, y-VELOCITY, x+42,  y-18-VELOCITY);
+    r.Set(x + 22, y - VELOCITY, x + 42, y - 18 - VELOCITY);
     if (!CanWalk(r)) {
       return EFalse;
     }
-//    if (mSprite->y - VELOCITY < 0 || mPlayfield->IsWall(mSprite->x + 22, mSprite->y - 18 - VELOCITY) ||
-//        mPlayfield->IsWall(mSprite->x + 42, mSprite->y - 18 - VELOCITY)) {
-//      return EFalse;
-//    }
+    //    if (mSprite->y - VELOCITY < 0 || mPlayfield->IsWall(mSprite->x + 22,
+    //    mSprite->y - 18 - VELOCITY) ||
+    //        mPlayfield->IsWall(mSprite->x + 42, mSprite->y - 18 - VELOCITY)) {
+    //      return EFalse;
+    //    }
     if (mState != WALK_STATE || mSprite->mDirection != DIRECTION_UP) {
       NewState(WALK_STATE, DIRECTION_UP);
     }
     return ETrue;
   }
-  
+
   if (gControls.IsPressed(JOYDOWN)) {
     if (MaybeFall()) {
       return EFalse;
     }
-    r.Set(x+22, y+VELOCITY, x+42, y+VELOCITY);
+    r.Set(x + 22, y + VELOCITY, x + 42, y + VELOCITY);
     if (!CanWalk(r)) {
       return EFalse;
     }
-//    if (mPlayfield->IsWall(mSprite->x + 22, mSprite->y + VELOCITY) ||
-//        mPlayfield->IsWall(mSprite->x + 42, mSprite->y + VELOCITY)) {
-//      return EFalse;
-//    }
+    //    if (mPlayfield->IsWall(mSprite->x + 22, mSprite->y + VELOCITY) ||
+    //        mPlayfield->IsWall(mSprite->x + 42, mSprite->y + VELOCITY)) {
+    //      return EFalse;
+    //    }
     if (mState != WALK_STATE || mSprite->mDirection != DIRECTION_DOWN) {
       NewState(WALK_STATE, DIRECTION_DOWN);
     }
     return ETrue;
   }
-  
+
   return EFalse;
 }
 
@@ -257,13 +278,13 @@ TBool GPlayerProcess::WalkState() {
   if (MaybeSword()) {
     return ETrue;
   }
-  
+
   // maybe change direction!
   if (!MaybeWalk()) {
     NewState(IDLE_STATE, mSprite->mDirection);
     return ETrue;
   }
-  
+
   // collision?
   if (MaybeHit()) {
     // bounce player off enemy
@@ -272,7 +293,7 @@ TBool GPlayerProcess::WalkState() {
     NewState(IDLE_STATE, mSprite->mDirection);
     return ETrue;
   }
-  
+
   if (MaybeFall()) {
     NewState(FALL_STATE, DIRECTION_DOWN);
     return ETrue;
@@ -280,8 +301,10 @@ TBool GPlayerProcess::WalkState() {
   // can player keep walking?
   switch (mSprite->mDirection) {
     case DIRECTION_LEFT:
-//      if (mPlayfield->IsWall(mSprite->x + 22 + mSprite->vx, mSprite->y + mSprite->vy) ||
-//          mPlayfield->IsWall(mSprite->x + 22 + mSprite->vx, mSprite->y - 18 + mSprite->vy)) {
+      //      if (mPlayfield->IsWall(mSprite->x + 22 + mSprite->vx, mSprite->y +
+      //      mSprite->vy) ||
+      //          mPlayfield->IsWall(mSprite->x + 22 + mSprite->vx, mSprite->y -
+      //          18 + mSprite->vy)) {
       if (mPlayfield->IsWall(
         mSprite->x + 26 + mSprite->vx, mSprite->y - 2 + mSprite->vy) ||
           mPlayfield->IsWall(
@@ -291,8 +314,10 @@ TBool GPlayerProcess::WalkState() {
       }
       break;
     case DIRECTION_RIGHT:
-//      if (mPlayfield->IsWall(mSprite->x + 42 + mSprite->vx, mSprite->y + mSprite->vy) ||
-//          mPlayfield->IsWall(mSprite->x + 42 + mSprite->vx, mSprite->y - 18 + mSprite->vy)) {
+      //      if (mPlayfield->IsWall(mSprite->x + 42 + mSprite->vx, mSprite->y +
+      //      mSprite->vy) ||
+      //          mPlayfield->IsWall(mSprite->x + 42 + mSprite->vx, mSprite->y -
+      //          18 + mSprite->vy)) {
       if (mPlayfield->IsWall(
         mSprite->x + 38 + mSprite->vx, mSprite->y - 2 + mSprite->vy) ||
           mPlayfield->IsWall(
@@ -357,10 +382,9 @@ TBool GPlayerProcess::RunBefore() {
 TBool GPlayerProcess::RunAfter() {
   // position viewport to follow player
   const TFloat xx = mSprite->x - gViewPort->mWorldX,
-      yy = mSprite->y - gViewPort->mWorldY,
-      dx = gViewPort->mRect.Width(),
-      dy = gViewPort->mRect.Height();
-  
+               yy = mSprite->y - gViewPort->mWorldY,
+               dx = gViewPort->mRect.Width(), dy = gViewPort->mRect.Height();
+
   // Don't go less than 0 or more than map width/height
   if (xx > dx) {
     gViewPort->mWorldX = MIN(mGameState->MapWidth(), gViewPort->mWorldX + dx);
@@ -371,6 +395,6 @@ TBool GPlayerProcess::RunAfter() {
   } else if (yy < 0) {
     gViewPort->mWorldY = MAX(0, gViewPort->mWorldY - dy);
   }
-  
+
   return ETrue;
 }
