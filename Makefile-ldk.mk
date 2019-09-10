23b3196adfaefdab90dec5eb8952e13cff86370b
jgarcia <jesusdgarcia@gmail.com>
2019-09-10 16:52:48 -0400

    Updated Makefile-ldk.mk

--- Makefile-ldk.mk
+++ Makefile-ldk.mk
@@ -8,29 +8,26 @@ LD_COMMAND = $(CHAINPREFIX)/mipsel-buildroot-linux-uclibc/bin/ld
 # ----------- SETUP VARS FOR SOURCES --------------
 
 ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
-BUILD_ROOT = $(ROOT_DIR)/build
-OBJS_DIR = $(BUILD_ROOT)/objs
-BUILD_DIR = $(BUILD_ROOT)/ldk
-
-
-GAME_SOURCE_DIR = src
-LDK_SOURCE_DIR = src/LDK
-CREATIVE_ENGINE_PATH = creative-engine
+BUILD_DIR = $(ROOT_DIR)/build/ldk
+GAME_SOURCE_DIR = $(ROOT_DIR)/src
+LDK_SOURCE_DIR = $(ROOT_DIR)/src/LDK
+CREATIVE_ENGINE_PATH = $(ROOT_DIR)/creative-engine
 CREATIVE_ENGINE_SOURCE_DIR = $(CREATIVE_ENGINE_PATH)/src
 
 
 TARGET = $(BUILD_DIR)/modite-adventure.dge
 
-# SOURCES = $(shell find ${GAME_SOURCE_DIR} -type f -name '*.cpp')
-# SOURCES += $(shell find $(CREATIVE_ENGINE_SOURCE_DIR) -type f -name '*.c*')
-SOURCES := $(shell find ${GAME_SOURCE_DIR} ${CREATIVE_ENGINE_SOURCE_DIR} -type f \( -name '*.c' -o -name '*.cpp' \))
+SOURCES = $(shell find ${GAME_SOURCE_DIR} -type f -name '*.cpp')
+SOURCES += $(shell find $(CREATIVE_ENGINE_SOURCE_DIR) -type f -name '*.c*')
 
 RESOURCES_BIN = $(GAME_SOURCE_DIR)/Resources.bin
 RESOURCES_BIN_O = $(BUILD_DIR)/Resources.bin
 
-$(shell mkdir -p $(BUILD_DIR) $(OBJ_DIR))
 
-$(shell ${LD_COMMAND} -r -b binary -o $(RESOURCES_BIN_O) $(RESOURCES_BIN))
+$(shell mkdir -p $(BUILD_DIR))
+
+
+# $(shell ${LD_COMMAND} -r -b binary -o $(RESOURCES_BIN_O) $(RESOURCES_BIN))
 
 
 
@@ -42,6 +39,7 @@ INCLUDE = -I $(CREATIVE_ENGINE_SOURCE_DIR) \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/Display \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/Display/LDKDisplay \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/Controls \
+          -I $(CREATIVE_ENGINE_SOURCE_DIR)/Controls/DesktopControls \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/Controls/LDKControls \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/Widgets \
           -I $(CREATIVE_ENGINE_SOURCE_DIR)/libxmp \
@@ -57,6 +55,9 @@ INCLUDE = -I $(CREATIVE_ENGINE_SOURCE_DIR) \
           -I $(GAME_SOURCE_DIR)/GameMenuState \
 
 
+
+CPP_OBJS = $(SOURCES:.cpp=.o)
+OBJS += $(CPP_OBJS:.c=.o)
 
 
 CXX = $(CROSS_COMPILE)-g++
@@ -79,34 +80,25 @@ CFLAGS += -fprofile-use -fprofile-dir=./profile -DNO_ROM_BROWSER
 CXXFLAGS = $(CFLAGS) -std=gnu++14 -fno-exceptions -fno-rtti -fno-math-errno -fno-threadsafe-statics
 LDFLAGS = $(SDL_LIBS) -lSDL_mixer -lSDL_image -lSDL_gfx -flto  -s
 
+#test 123
 
-CPP_OBJS = $(SOURCES:.cpp=.o)
-ALL_OBJS += $(CPP_OBJS:.c=.o)
+DEPLOY_TMP = /tmp/.modus-ipk/root/home/retrofw/test
 
-OBJS = $(ALL_OBJS:src/%.o=$(OBJS_DIR)/%.o)
+all: $(TARGET)
 
-
-## TODO: @jgarcia Implement objdir
-# https://github.com/zfteam/gmenu2x-gameshell/blob/master/Makefile
-
-all: $(OBJS) $(TARGET)
-
-$(TARGET): $(OBJS)
-	$(CMD)$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $@
-
-$(OBJS): %.cpp %c
-	$(CXX) $(CXXFLAGS) $< -o $@
+$(TARGET) : $(OBJS)
+	$(CMD)$(CXX) $(CXXFLAGS) $^ $(LDFLAGS) -o $@
 
 
 ipk: $(TARGET)
 	@echo "Packaging..."
-	./package.sh
+	${ROOT_DIR}/scripts/package.sh
 .PHONY: ipk
 
 
 clean:
 	#rm -f $(TARGET); find -L . -name "*.o" -exec rm {} \;
-	@rm -f $(TARGET); find . -name "*.o" -exec rm {} \;
+	@rm -f $(TARGET); find ${ROOT_DIR} -name "*.o" -exec rm {} \;
 
 .PHONY: clean
 
@@ -117,7 +109,7 @@ cleanall: cleanobjects
 
 
 cleanobjects:
-	find -L . -name "*.o" -exec rm {} \;
+	find -L ${ROOT_DIR} -name "*.o" -exec rm {} \;
 .PHONY: cleanobjects
 
 
