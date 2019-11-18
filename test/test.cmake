08bfc275bcdaa6cfcdb736fb86eeb9ebca916e6b
dranderson <4012994+dranderson@users.noreply.github.com>
2019-11-18 13:04:20 -0500

    Update test.cmake

--- test/test.cmake
+++ test/test.cmake
@@ -8,6 +8,29 @@ ADD_COMPILE_DEFINITIONS(__MODUS_TARGET_DESKTOP_CONTROLS__=true)
 ADD_COMPILE_DEFINITIONS(__MODUS_TARGET_DESKTOP_DISPLAY__=true)
 ADD_COMPILE_DEFINITIONS(LIBXMP_CORE_PLAYER=true)
 
+########## RCOMP #########
+
+# resource compiler
+SET(RCOMP "${CREATIVE_ENGINE_PATH}/tools/rcomp")
+
+# build rcomp-src
+ADD_CUSTOM_COMMAND(
+        OUTPUT rcomp
+        COMMAND cd ${CREATIVE_ENGINE_PATH}/tools/rcomp-src && $(MAKE)  # $(MAKE) passes -j to make
+        COMMENT "Building rcomp ${CREATIVE_ENGINE_PATH}"
+)
+
+# build Resources.bin
+ADD_CUSTOM_COMMAND(
+        OUTPUT Resources.bin
+        COMMAND find ${CMAKE_CURRENT_SOURCE_DIR} -name "Resources.bin" -exec rm -f {} +
+        COMMAND find ${CMAKE_CURRENT_BINARY_DIR} -name "BResourceManager.cpp.o" -exec rm -f {} +
+        COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR}/src && ${RCOMP} Resources.r
+        DEPENDS rcomp
+        COMMENT "Compiling Resources ${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+
 FILE(GLOB_RECURSE TESTS "${CMAKE_CURRENT_SOURCE_DIR}/test/src/tests/*.cpp")
 ADD_EXECUTABLE(test
         ${CMAKE_CURRENT_SOURCE_DIR}/test/src/main.cpp
