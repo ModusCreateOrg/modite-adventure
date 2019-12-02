3505c7b0f393504879836689dca8cafa7bb5cfe6
dranderson <4012994+dranderson@users.noreply.github.com>
2019-12-02 14:45:46 -0500

    Update make-ldk-ci.sh

--- scripts/make-ldk-ci.sh
+++ scripts/make-ldk-ci.sh
@@ -15,35 +15,4 @@ if [[ $? -eq 0 ]]; then
  	make -f Makefile-ldk.mk cleanobjects >/dev/null 2>&1
 fi
 
-me=`whoami`
-
-
-ping 169.254.1.1 -c 1 -W 1 -i 200
-# RC=$?
-
-# if [[ $RC -gt 0 ]]; then
-#   PRINT_YELLOW "WARNING: LDK_IP not connected!"
-#   echo "Connect LDK_IP and "
-
-#   while true; do
-#     echo "Checking for ${LDK_IP} `date`"
-
-#     ping 169.254.1.1  -c 1 -W 0 -i 200 >/dev/null 2>&1
-#     RC=$?
-
-#     if [[ $RC -eq 0 ]]; then
-#       break;
-#     fi
-#   done
-
-#   PRINT_GREEN "Found LDK_IP. Deploying..."
-# fi
-
-
-md5=`md5sum build/ldk/modite-adventure.dge | awk '{print $1}'`
-md5=${md5: -6}
-
-#make -f Makefile-ldk.mk cleanall >/dev/null 2>&1
-make -f Makefile-ldk.mk cleanall
-
 
