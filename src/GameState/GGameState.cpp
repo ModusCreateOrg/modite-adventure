d4e26057a7c710ee21127dc7d27ed2abcea2c9a5
Michael Tintiuc <contact@michaeltintiuc.com>
2020-01-11 22:50:27 +0200

    Fixes #397. Resume after death, exit dungeon

--- src/GameState/GGameState.cpp
+++ src/GameState/GGameState.cpp
@@ -85,15 +85,37 @@ GProcess *GGameState::AddProcess(GProcess *p) {
   return p;
 }
 
-void GGameState::TryAgain() {
+void GGameState::TryAgain(TBool aExitDungeon) {
   if (mGameOver) {
     delete mGameOver;
     mGameOver = ENull;
   }
   GPlayer::mGameOver = EFalse;
   GPlayer::mHitPoints = GPlayer::mMaxHitPoints;
-  //  mLevel = -1;
-  //  NextLevel(DUNGEON_DEV, 2);
+
+  if (aExitDungeon) {
+    NextLevel(OVERWORLD_DUNGEON, LastOverworldLevel());
+    return;
+  }
+
+  // Cache previous respawn coords, LoadLevel resets them
+  TFloat respawnAt[2] = { GPlayerProcess::mRespawnAt[0], GPlayerProcess::mRespawnAt[1] };
+
+  // Reset the level
+  LoadLevel(mName, mLevel, mTileMapId);
+
+  // Restore respawn coords
+  GPlayerProcess::mRespawnAt[0] = respawnAt[0];
+  GPlayerProcess::mRespawnAt[1] = respawnAt[1];
+
+  if (respawnAt[0] != '\0') {
+    GAnchorSprite *s = GPlayer::mProcess->Sprite();
+    s->x = respawnAt[0];
+    s->y = respawnAt[1];
+#ifdef DEBUG_MODE
+    printf("RESPAWNED AT %f %f\n", respawnAt[0], respawnAt[1]);
+#endif
+  }
 }
 
 /*******************************************************************************
--- src/GameState/player/GGameOver.cpp
+++ src/GameState/player/GGameOver.cpp
@@ -20,14 +20,7 @@ GGameOver::GGameOver(GGameState *aGameState) {
   gDisplay.SetColor(COLOR_TEXT, 255, 255, 255);
   gDisplay.SetColor(COLOR_TEXT_BG, 0, 0, 0);
 
-#ifdef DEBUG_MODE
   mState = 0;
-#else
-  mState = 1;
-#endif
-
-  // Load saved games
-  gSavedGameList.LoadSavedGameList();
 }
 
 GGameOver::~GGameOver() {
@@ -62,6 +55,7 @@ static void center(const char *s, TInt y, TBool inverse = EFalse) {
 TBool GGameOver::Run() {
   BBitmap *b = gDisplay.renderBitmap;
   TInt x = 4, y = 10, line_height = 18;
+  TInt statesMax = 1;
 
   gDisplay.SetColor(COLOR_TEXT, 255, 255, 255);
   gDisplay.SetColor(COLOR_TEXT_BG, 0, 0, 0);
@@ -69,30 +63,23 @@ TBool GGameOver::Run() {
   center("You are dead", y);
   y += line_height * 2;
 
-#ifdef DEBUG_MODE
-  center("Resume Game (debug)", y, mState == 0);
-  y += line_height;
-#endif
-
-  center("Load Saved Game", y, mState == 1);
+  center("Try again", y, mState == 0);
   y += line_height;
 
-  center("Exit", y, mState == 2);
+  if (mGameState->Dungeon() != OVERWORLD_DUNGEON) {
+    center("Leave dungeon", y, mState == 1);
+    y += line_height;
+    statesMax = 2;
+  }
+
+  center("Exit", y, mState == statesMax);
 
   if (gControls.WasPressed(JOYUP)) {
-#ifdef DEBUG_MODE
-    mState = CLAMP(mState - 1, 0, 2);
-#else
-    mState = CLAMP(mState - 1, 1, 2);
-#endif
+    mState = CLAMP(mState - 1, 0, statesMax);
   }
 
   if (gControls.WasPressed(JOYDOWN)) {
-#ifdef DEBUG_MODE
-    mState = CLAMP(mState + 1, 0, 2);
-#else
-    mState = CLAMP(mState + 1, 1, 2);
-#endif
+    mState = CLAMP(mState + 1, 0, statesMax);
   }
 
   if (gControls.WasPressed(BUTTON_SELECT | BUTTON_START | BUTTONA)) {
@@ -101,27 +88,12 @@ TBool GGameOver::Run() {
       TRGB c = mSavedPalette[color];
       gDisplay.SetColor(color, c);
     }
-    switch (mState) {
-      case 0:
-        mGameState->TryAgain();
-        gControls.Reset();
-        break;
-      case 1:
-        mGameState->TryAgain();
-        gControls.Reset();
 
-        if (gSavedGameList.mNumSavedGames > 0) {
-          char saveGameName[256];
-          gSavedGameList.First()->GameName(saveGameName);
-          gGame->StartGame(saveGameName);
-        } else {
-          gGame->SetState(GAME_STATE_RESET_GAME);
-        }
-        return EFalse;
-      case 2:
-      default:
-        gGame->SetState(GAME_STATE_MAIN_MENU);
-        break;
+    if (mState < statesMax) {
+      mGameState->TryAgain(mState > 0);
+      gControls.Reset();
+    } else {
+      gGame->SetState(GAME_STATE_MAIN_MENU);
     }
   }
 
