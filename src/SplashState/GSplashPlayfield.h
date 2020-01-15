0dfeeee37ac4c0026fd58b53eb08b5075d02afdd
jgarcia <jesusdgarcia@gmail.com>
2020-01-14 22:27:59 -0500

    First pass

--- src/SplashState/GSplashPlayfield.h
+++ src/SplashState/GSplashPlayfield.h
@@ -3,16 +3,33 @@
 
 #include "Game.h"
 
+typedef struct {
+  TInt index;
+  float x, y, z;
+  float screen_x, screen_y;
+  TUint16 color;
+} Particle;
+
+
 class GSplashPlayfield : public BPlayfield {
 public:
   GSplashPlayfield();
 
   virtual ~GSplashPlayfield();
 
-  void Render();
+  void Render() final;
+  void Animate() final;
+
+  void InitParticles();
 
 public:
   BBitmap *mBackground;
+  TRGB mSavedPalette[256];
+  TInt mNumParticles;
+  Particle *mParticles;
+  TInt16 mNumParticlesMoved;
+  TUint16 mFrame;
+  TFloat mAnimatedColorValue;
 };
 
 #endif //MODITE_GSPLASHPLAYFIELD_H
--- src/SplashState/GSplashState.cpp
+++ src/SplashState/GSplashState.cpp
@@ -6,12 +6,12 @@
 GSplashState::GSplashState() : BGameEngine(gViewPort) {
   mPlayfield = new GSplashPlayfield();
   AddProcess(new GSplashProcess());
-  AddProcess(new GDropProcess(this, 0));
-  AddProcess(new GDropProcess(this, 1));
-  AddProcess(new GDropProcess(this, 2));
-  AddProcess(new GDropProcess(this, 3));
-  AddProcess(new GDropProcess(this, 4));
-  AddProcess(new GDropProcess(this, 5));
+//  AddProcess(new GDropProcess(this, 0));
+//  AddProcess(new GDropProcess(this, 1));
+//  AddProcess(new GDropProcess(this, 2));
+//  AddProcess(new GDropProcess(this, 3));
+//  AddProcess(new GDropProcess(this, 4));
+//  AddProcess(new GDropProcess(this, 5));
 }
 
 GSplashState::~GSplashState() {
