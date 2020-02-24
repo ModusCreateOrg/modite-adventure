b95fab8f6ab1238f8ec9e6a8d33242cab7c33568
Aaron-Goldman <aaron.goldman@moduscreate.com>
2020-02-24 16:27:46 -0500

    finalize alignment

--- src/GameState/inventory/GInventory.cpp
+++ src/GameState/inventory/GInventory.cpp
@@ -21,6 +21,8 @@ GInventory::GInventory(BViewPort *aViewPort) : BGameEngine(aViewPort) {
   mCurrentColumn = mCurrentRow = 0;
   gDisplay.SetColor(COLOR_TEXT_BG, 0, 0, 0);
   gDisplay.SetColor(COLOR_WHITE, 255, 255, 255);
+  gDisplay.SetColor(COLOR_TEXT, 255, 255, 255);
+  gDisplay.SetColor(COLOR_METER_OUTLINE, 64, 64, 64);
 }
 
 
@@ -28,52 +30,59 @@ GInventory::GInventory(BViewPort *aViewPort) : BGameEngine(aViewPort) {
 GInventory::~GInventory() {
 }
 
-static void render_meter(BViewPort *vp, BBitmap *screen, TUint8 color, TInt x, TInt y, TInt value, TInt max) {
+static void render_meter(BViewPort *vp, BBitmap *bm, const char *name, TUint8 color, TInt x, TInt y, TInt value, TInt max) {
+  bm->DrawString(vp, name, gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
+  x += 56, y -= 2;
+
   TFloat pct = TFloat(value) / max;
 
   const TInt innerWidth = METER_WIDTH - 2;
 
   TInt w = (TInt)(pct *  innerWidth);
 
-  screen->DrawRect(vp, x, y, x + METER_WIDTH - 1, y + METER_HEIGHT - 1, COLOR_METER_OUTLINE);
-  screen->DrawFastHLine(vp, x + 1, y + 1, w, color);
-  screen->FillRect(vp, x + 1, y + 2, x + w, y + METER_HEIGHT - 2, TUint8(color + 1));
+  bm->DrawRect(vp, x, y, x + METER_WIDTH - 1, y + METER_HEIGHT - 1, COLOR_METER_OUTLINE);
+  bm->DrawFastHLine(vp, x + 1, y + 1, w, color);
+  bm->FillRect(vp, x + 1, y + 2, x + w, y + METER_HEIGHT - 2, TUint8(color + 1));
 
   // erase the corners
-  screen->DrawFastHLine(vp, x, y, 2, COLOR_HUD_BG);
-  screen->DrawFastVLine(vp, x, y, 2, COLOR_HUD_BG);
+  bm->DrawFastHLine(vp, x, y, 2, COLOR_TEXT_BG);
+  bm->DrawFastVLine(vp, x, y, 2, COLOR_TEXT_BG);
 
-  screen->DrawFastHLine(vp, x + METER_WIDTH - 2, y, 2, COLOR_HUD_BG);
-  screen->DrawFastVLine(vp, x + METER_WIDTH - 1, y, 2, COLOR_HUD_BG);
+  bm->DrawFastHLine(vp, x + METER_WIDTH - 2, y, 2, COLOR_TEXT_BG);
+  bm->DrawFastVLine(vp, x + METER_WIDTH - 1, y, 2, COLOR_TEXT_BG);
 
-  screen->DrawFastHLine(vp, x, y + METER_HEIGHT - 1, 2, COLOR_HUD_BG);
-  screen->DrawFastVLine(vp, x, y + METER_HEIGHT - 2, 2, COLOR_HUD_BG);
+  bm->DrawFastHLine(vp, x, y + METER_HEIGHT - 1, 2, COLOR_TEXT_BG);
+  bm->DrawFastVLine(vp, x, y + METER_HEIGHT - 2, 2, COLOR_TEXT_BG);
 
-  screen->DrawFastHLine(vp, x + METER_WIDTH - 2, y + METER_HEIGHT - 1, 2, COLOR_HUD_BG);
-  screen->DrawFastVLine(vp, x + METER_WIDTH - 1, y + METER_HEIGHT - 2, 2, COLOR_HUD_BG);
+  bm->DrawFastHLine(vp, x + METER_WIDTH - 2, y + METER_HEIGHT - 1, 2, COLOR_TEXT_BG);
+  bm->DrawFastVLine(vp, x + METER_WIDTH - 1, y + METER_HEIGHT - 2, 2, COLOR_TEXT_BG);
 
   // draw the 4 pixels to round off the oval
-  screen->WritePixel(x + 1, y + 1, COLOR_METER_OUTLINE);
-  screen->WritePixel(x + METER_WIDTH - 2, y + 1, COLOR_METER_OUTLINE);
+  bm->WritePixel(x + 1, y + 1, COLOR_METER_OUTLINE);
+  bm->WritePixel(x + METER_WIDTH - 2, y + 1, COLOR_METER_OUTLINE);
 
-  screen->WritePixel(x + 1, y + METER_HEIGHT - 2, COLOR_METER_OUTLINE);
-  screen->WritePixel(x + METER_WIDTH - 2, y + METER_HEIGHT - 2, COLOR_METER_OUTLINE);
+  bm->WritePixel(x + 1, y + METER_HEIGHT - 2, COLOR_METER_OUTLINE);
+  bm->WritePixel(x + METER_WIDTH - 2, y + METER_HEIGHT - 2, COLOR_METER_OUTLINE);
 
   char buf[16];
-  sprintf(buf, "%d/%d", value, max);
-  screen->DrawStringShadow(vp, buf, gFont8x8, x + 20, y + 3, COLOR_TEXT, COLOR_TEXT_SHADOW, COLOR_TEXT_TRANSPARENT);
+  TInt length = 0;
+  sprintf(buf, "%d/%d%n", value, max, &length);
+  // center-align text
+  x += METER_WIDTH / 2 - 4 * length;
+  bm->DrawStringShadow(vp, buf, gFont8x8, x, y + 3, COLOR_TEXT, COLOR_TEXT_SHADOW, COLOR_TEXT_TRANSPARENT);
 }
 
 static void render_item_slot(BViewPort *aViewPort, TUint16 aItemNumber, TInt aX, TInt aY, TBool aIsSelected, TBool aShowCounter = EFalse) {
   BBitmap *bm = gDisplay.renderBitmap;
+  TInt i = GPlayer::mInventoryList.ItemCount(aItemNumber);
   if (aShowCounter) {
-    TInt i = GPlayer::mInventoryList.ItemCount(aItemNumber);
     char buf[16];
-    sprintf(buf, "x%d", i);
-    bm->DrawString(aViewPort, buf, gFont8x8, aX + 8, aY + 36, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
+    TInt length = 0;
+    sprintf(buf, "x%d%n", i, &length);
+    bm->DrawString(aViewPort, buf, gFont8x8, aX + 16 - 4 * length, aY + 36, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
   }
 
-  if (aShowCounter || GPlayer::mInventoryList.FindItem(aItemNumber)) {
+  if (i) {
     TInt16 img = items[aItemNumber];
 
     BBitmap *env = gResourceManager.GetBitmap(ENVIRONMENT_SLOT);
@@ -91,10 +100,7 @@ static void render_item_slot(BViewPort *aViewPort, TUint16 aItemNumber, TInt aX,
   }
   TUint8 color = aIsSelected ? COLOR_WHITE : COLOR_METER_OUTLINE;
 
-  bm->DrawFastHLine(aViewPort, aX, aY, 32, color);
-  bm->DrawFastHLine(aViewPort, aX, aY + 31, 32, color);
-  bm->DrawFastVLine(aViewPort, aX, aY + 1, 30, color);
-  bm->DrawFastVLine(aViewPort, aX + 31, aY + 1, 30, color);
+  bm->DrawRect(aViewPort, aX, aY, aX + 31, aY + 31, color);
 }
 
 static void render_item_row(BViewPort *aViewPort, const char *aHeading, const TUint16 *aItemRow, TInt aX, TInt aY, TInt aSelected, TInt aEquipped, TBool aShowCounter = EFalse) {
@@ -105,11 +111,8 @@ static void render_item_row(BViewPort *aViewPort, const char *aHeading, const TU
   for (TInt col = 0; col < 4; col++) {
     render_item_slot(aViewPort, aItemRow[col], aX, aY, aSelected == col, aShowCounter);
     if (aEquipped - 1 == col) {
-      // draw corners of square
-      bm->DrawFastHLine(aViewPort, aX - 2, aY - 2, 36, COLOR_WHITE);
-      bm->DrawFastHLine(aViewPort, aX - 2, aY + 33, 36, COLOR_WHITE);
-      bm->DrawFastVLine(aViewPort, aX - 2, aY - 1, 34, COLOR_WHITE);
-      bm->DrawFastVLine(aViewPort, aX + 33, aY - 1, 34, COLOR_WHITE);
+      bm->DrawRect(aViewPort, aX - 2, aY - 2, aX + 33, aY + 33, COLOR_WHITE);
+      // erase middles of edges from square
       bm->DrawFastHLine(aViewPort, aX + 8, aY - 2, 16, COLOR_TEXT_BG);
       bm->DrawFastHLine(aViewPort, aX + 8, aY + 33, 16, COLOR_TEXT_BG);
       bm->DrawFastVLine(aViewPort, aX - 2, aY + 8, 16, COLOR_TEXT_BG);
@@ -121,6 +124,8 @@ static void render_item_row(BViewPort *aViewPort, const char *aHeading, const TU
 
 void GInventory::RenderInventory() {
   BBitmap *bm = gDisplay.renderBitmap;
+  char buf[256];
+
   TInt x = 4, y = 4;
   render_item_row(mViewPort, "Potions", ITEM_LAYOUT[0], x, y, mCurrentRow == 0 ? mCurrentColumn : -1, 0, ETrue);
   y += 60;
@@ -131,72 +136,49 @@ void GInventory::RenderInventory() {
   render_item_row(mViewPort, "Ring", ITEM_LAYOUT[3], x, y, mCurrentRow == 3 ? mCurrentColumn : -1, GPlayer::mEquipped.mRingElement);
 
   x = 160, y = 4;
-  char buf[256];
   sprintf(buf, "Level: %d", GPlayer::mLevel);
   bm->DrawString(mViewPort, buf, gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
   y += 20;
-  bm->DrawString(mViewPort, "Health", gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
-  render_meter(mViewPort, bm, COLOR_HEALTH, x + 56, y - 2, GPlayer::mHitPoints, GPlayer::mMaxHitPoints);
+  render_meter(mViewPort, bm, "Health", COLOR_HEALTH, x, y, GPlayer::mHitPoints, GPlayer::mMaxHitPoints);
   y += 20;
-  bm->DrawString(mViewPort, "Magic", gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
-  render_meter(mViewPort, bm, COLOR_MAGIC, x + 56, y - 2, GPlayer::mManaPotion, 100);
+  render_meter(mViewPort, bm, "Magic", COLOR_MAGIC, x, y, GPlayer::mManaPotion, 100);
   y += 20;
   sprintf(buf, "Attack: %d", GPlayer::mAttackStrength);
   bm->DrawString(mViewPort, buf, gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
 
-  x = SCREEN_WIDTH - 40;
+  y += 20;
+  bm->DrawRect(mViewPort, x, y, x + 111, y + 95, COLOR_METER_OUTLINE);
+
+  TInt offsetX, offsetY;
+  TRect srcRect;
+  BSpriteInfo *info = GPlayer::mSprite->mSpriteSheet->GetSpriteInfo(GPlayer::mSprite->mImageNumber);
+  BBitmap *playerBitmap = gResourceManager.GetBitmap(PLAYER_SLOT);
+  srcRect.x1 = info->x1;
+  srcRect.y1 = info->y1;
+  srcRect.x2 = info->x2;
+  srcRect.y2 = info->y2;
+  offsetX = info->dx + TInt(GPlayer::mSprite->mDx) + 24;
+  offsetY = info->dy + TInt(GPlayer::mSprite->mDy) + 4;
+  bm->DrawBitmapTransparent(mViewPort, playerBitmap, srcRect, x + offsetX, y + offsetY, GPlayer::mSprite->flags >> 6u);
+
+  y = 194;
+  render_meter(mViewPort, bm, "Exp.", COLOR_EXPERIENCE, x, y, GPlayer::mExperience, GPlayer::mNextLevel);
+
+  x = SCREEN_WIDTH - 40, y = 80;
   render_item_slot(mViewPort, ITEM_GLOVES, x, y, EFalse);
   y += 36;
   render_item_slot(mViewPort, ITEM_SWORD, x, y, EFalse);
   y += 36;
   render_item_slot(mViewPort, ITEM_BOOTS, x, y, EFalse);
 
-  x = 160, y = 192;
-  bm->DrawString(mViewPort, "Exp.", gFont8x8, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT);
-  render_meter(mViewPort, bm, COLOR_EXPERIENCE, x + 56, y - 2, GPlayer::mExperience, GPlayer::mNextLevel);
-  TRect srcRect;
-  BSpriteInfo *info = GPlayer::mSprite->mSpriteSheet->GetSpriteInfo(GPlayer::mSprite->mImageNumber);
-  srcRect.x1 = info->x1;
-  srcRect.y1 = info->y1;
-  srcRect.x2 = info->x2;
-  srcRect.y2 = info->y2;
-  bm->DrawBitmapTransparent(mViewPort, gResourceManager.GetBitmap(PLAYER_SLOT), srcRect, 200, 120, GPlayer::mSprite->flags >> 6u);
-
-  /*
-  for (GInventoryItem *i = GPlayer::mInventoryList.First(); !GPlayer::mInventoryList.End( i); i = GPlayer::mInventoryList.Next(i)) {
-    if (count >= mTop && rendered < RENDER_ITEM_MAX) {
-      TRect srcRect;
-      srcRect.x1 = (i->mImage % pitch) * bw;
-      srcRect.x2 = srcRect.x1 + bw - 1;
-      srcRect.y1 = (i->mImage / pitch) * bh;
-      srcRect.y2 = srcRect.y1 + bh - 1;
-
-      if (i->mCount > 1) {
-        sprintf(buf, "%2d %-18s", i->mCount, itemNames[i->mItemNumber]); // , i->mImage);
-      }
-      else {
-        sprintf(buf, "   %-18s", itemNames[i->mItemNumber]); // , i->mImage);
-      }
-      if (count == mCurrent) {
-        selected = i;
-        bm->FillRect(mViewPort, 0, y, SCREEN_WIDTH - 1, y + 17, COLOR_TEXT);
-        bm->DrawBitmapTransparent(mViewPort, env, srcRect, 0, y - 7);
-        bm->DrawString(mViewPort, buf, gFont16x16, x, y + 1, COLOR_TEXT_BG, COLOR_TEXT_TRANSPARENT, -4);
-      }
-      else {
-        bm->DrawBitmapTransparent(mViewPort, env, srcRect, 0, y - 7);
-        bm->DrawString(mViewPort, buf, gFont16x16, x, y + 1, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -4);
-      }
-      rendered++;
-      y += 18;
-    }
-    count++;
-  }
-*/
   GInventoryItem* selected = GPlayer::mInventoryList.FindItem(ITEM_LAYOUT[mCurrentRow][mCurrentColumn]);
   if (selected) {
-    sprintf(buf, "A to use %s", itemNames[selected->mItemNumber]);
-    bm->DrawString(mViewPort, buf, gFont16x16, 4, 220, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -4);
+    if (mCurrentRow == 0) {
+      sprintf(buf, "A to use %s", itemNames[selected->mItemNumber]);
+    } else {
+      sprintf(buf, "A to equip %s", itemNames[selected->mItemNumber]);
+    }
+    bm->DrawString(mViewPort, buf, gFont16x16, 4, 220, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -5);
   }
   if (gControls.WasPressed(BUTTONA) && selected) {
     switch (selected->mItemNumber) {
@@ -311,72 +293,13 @@ void GInventory::RenderInventory() {
     }
   }
 }
-/*
-static void render_equipped_item(const char *aSlot, GInventoryItem *aItem, BViewPort *aViewPort, TInt x, TInt y) {
-  char buf[256];
-  BBitmap *bm = gDisplay.renderBitmap;
 
-  if (!aItem) {
-    sprintf(buf, "%8s:  None", aSlot);
-    bm->DrawString(aViewPort, buf, gFont16x16, x, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -4);
-    return;
-  }
-
-  BBitmap *env = gResourceManager.GetBitmap(ENVIRONMENT_SLOT);
-  TInt bw = gResourceManager.BitmapWidth(ENVIRONMENT_SLOT),
-       bh = gResourceManager.BitmapHeight(ENVIRONMENT_SLOT),
-       pitch = env->Width() / bw;
-
-  TRect srcRect;
-  srcRect.x1 = (aItem->mImage % pitch) * bw;
-  srcRect.x2 = srcRect.x1 + bw - 1;
-  srcRect.y1 = (aItem->mImage / pitch) * bh;
-  srcRect.y2 = srcRect.y1 + bh - 1;
-
-  bm->DrawBitmapTransparent(aViewPort, env, srcRect, x + 13 * 8, y - 7);
-
-  sprintf(buf, "%8s:  %s", aSlot, itemNames[aItem->mItemNumber]);
-  bm->DrawString(aViewPort, buf, gFont16x16, x, y + 1, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -4);
-}
-
-void GInventory::RenderEquipped() {
-  TInt y = 8,
-       x = 8;
-
-  BBitmap *bm = gDisplay.renderBitmap;
-
-  bm->DrawString(mViewPort, "EQUIPPED", gFont16x16, 160 - 7 * 12 / 2, y, COLOR_TEXT, COLOR_TEXT_TRANSPARENT, -4);
-  y += 32;
-
-//  printf("**** TODO: Render inventory screen ***\n");
-
-  render_equipped_item("Weapon", GPlayer::mEquipped.mSword, mViewPort, x, y);
-  y += 18;
-  render_equipped_item("Spells", GPlayer::mEquipped.mSpellBook, mViewPort, x, y);
-  y += 18;
-  render_equipped_item("Amulet", GPlayer::mEquipped.mEarthAmulet, mViewPort, x, y);
-  y += 18;
-  render_equipped_item("Ring", GPlayer::mEquipped.mEarthRing, mViewPort, x, y);
-  y += 18;
-  render_equipped_item("Gloves", GPlayer::mEquipped.mGloves, mViewPort, x, y);
-  y += 18;
-  render_equipped_item("Boots", GPlayer::mEquipped.mBoots, mViewPort, x, y);
-  y += 18;
-
-  if (gControls.WasPressed(JOYRIGHT | JOYLEFT)) {
-    mMode = MODE_INVENTORY;
-  }
-}
-*/
 void GInventory::PostRender() {
   if (gControls.WasPressed(BUTTONR)) {
     gGame->ToggleInventory();
   }
 
   BBitmap *bm = gDisplay.renderBitmap;
-  bm->SetColor(COLOR_TEXT_BG, 0, 0, 0);
-  bm->SetColor(COLOR_TEXT, 255, 255, 255);
-
   bm->Clear(COLOR_TEXT_BG);
 
   RenderInventory();
