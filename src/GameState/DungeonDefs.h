b421e8b6898b04f30f356e50c229cfe633801baf
jaygarcia <jesusdgarcia@gmail.com>
2020-09-09 21:02:34 -0400

    setup for OW7 DGN4

--- src/GameState/DungeonDefs.h
+++ src/GameState/DungeonDefs.h
@@ -601,10 +601,10 @@ struct TDungeonInfo gDungeonDefs[] = {
       {
         OW7_DGN4_7168_OW7_DGN4_L1_MAP, // 0
         OW7_DGN4_7168_OW7_DGN4_L1_MAP, // 1
-        -1, //OW7_DGN4_7168_OW7_DGN4_L2_MAP, // 2
-        -1, //OW7_DGN4_7168_OW7_DGN4_L3_MAP, // 3
-        -1, //OW7_DGN4_7168_OW7_DGN4_L4_MAP, // 4
-        -1, //OW7_DGN4_7168_OW7_DGN4_L5_MAP, // 5
+        OW7_DGN4_7168_OW7_DGN4_L2_MAP, // 2
+        OW7_DGN4_7168_OW7_DGN4_L3_MAP, // 3
+        OW7_DGN4_7168_OW7_DGN4_L4_MAP, // 4
+        OW7_DGN4_7168_OW7_DGN4_L5_MAP, // 5
         -1, //OW7_DGN4_7168_OW7_DGN4_L6_MAP, // 6
         -1, //OW7_DGN4_7168_OW7_DGN4_L7_MAP, // 7
         -1, //OW7_DGN4_7168_OW7_DGN4_L8_MAP, // 8
