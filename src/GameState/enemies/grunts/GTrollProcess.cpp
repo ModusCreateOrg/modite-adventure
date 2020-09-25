9cd8ce9fdd13fb1ab547ec110127041f17b7845a
Jay Garcia <jesusdgarcia@gmail.com>
2020-09-24 23:09:45 -0400

    Various Enemy Tweaks (#619)
    * Centered shadow rect to sprite
    
    * Centered goblin sniper idle and made him faster!
    
    * Goblin now moves faster, has centered anims.
    
    * Orc now decides faster, has a better centered "shadow", and varying attack types return.
    
    * GBat - centered Y collision
    
    * GSpider decides faster, has faster taunt and moves a bit faster.
    
    * Gave Rat some love
    
    * Slime moves a bit faster and now has centered graphics
    
    * Faster Spider Taunt

--- src/GameState/enemies/grunts/GTrollProcess.cpp
+++ src/GameState/enemies/grunts/GTrollProcess.cpp
@@ -417,8 +417,8 @@ GTrollProcess::GTrollProcess(GGameState *aGameState, TInt aIp, TFloat aX, TFloat
   mRangeX = mRangeY = 16;
   mSprite->ClearFlags(SFLAG_RENDER_SHADOW); // Disable shadow
   mSprite->mSpriteSheet = gResourceManager.LoadSpriteSheet(CHARA_TROLL_BMP_SPRITES);
-  mSprite->cy = 0;
-  mSprite->h = 16;
+  mSprite->cy = 2;
+  mSprite->h = 18;
   mSprite->ResetShadow();
 }
 
