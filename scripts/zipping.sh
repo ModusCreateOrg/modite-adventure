b878838a65d2307f76637f35b9bfce18c3e68821
Tom Collins <thomas.collins@moduscreate.com>
2020-10-29 16:38:25 -0400

    making two different zip files for mac and linux os and releasing both

--- scripts/zipping.sh
+++ scripts/zipping.sh
@@ -3,7 +3,7 @@
 OS="$(uname)"
 
 if [ "$OS" == "Darwin" ]; then
-  cd /Users/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite.zip Modite.app/ && cd -
+  cd /Users/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite-mac.zip Modite.app/ && cd -
 elif [ "$OS" == "Linux" ]; then
-  cd /home/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite.zip Modite/ && cd -
+  cd /home/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite-linux.zip Modite/ && cd -
 fi
