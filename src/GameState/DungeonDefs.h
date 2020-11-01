6ae91fd019c314601a3d8cb81723c5ff46db1766
lgaribaldi <lucas.garibaldi.alves@gmail.com>
2020-11-01 17:37:37 -0300

    Enable OW3_DGN4

--- src/GameState/DungeonDefs.h
+++ src/GameState/DungeonDefs.h
@@ -265,9 +265,9 @@ struct TDungeonInfo gDungeonDefs[] = {
       {
         OW3_DGN4_3072_OW3_DGN4_L1_MAP, // 0
         OW3_DGN4_3072_OW3_DGN4_L1_MAP, // 1
-        -1, //OW3_DGN4_3072_OW3_DGN4_L2_MAP, // 2
-        -1, //OW3_DGN4_3072_OW3_DGN4_L3_MAP, // 3
-        -1, //OW3_DGN4_3072_OW3_DGN4_L4_MAP, // 4
+        OW3_DGN4_3072_OW3_DGN4_L2_MAP, // 2
+        OW3_DGN4_3072_OW3_DGN4_L3_MAP, // 3
+        OW3_DGN4_3072_OW3_DGN4_L4_MAP, // 4
         -1, //OW3_DGN4_3072_OW3_DGN4_L5_MAP, // 5
         -1, //OW3_DGN4_3072_OW3_DGN4_L6_MAP, // 6
         -1, //OW3_DGN4_3072_OW3_DGN4_L7_MAP, // 7
