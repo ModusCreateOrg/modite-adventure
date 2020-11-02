c09619669ee5d4052015a1ff576c932acf8cd6b2
Aaron-Goldman <aaron.goldman@moduscreate.com>
2020-11-02 16:44:51 -0500

    direct knockback away from center

--- src/GameState/enemies/final-boss/GFinalBossShieldProcess.cpp
+++ src/GameState/enemies/final-boss/GFinalBossShieldProcess.cpp
@@ -29,6 +29,10 @@ void GFinalBossShieldProcess::KillShield() {
     auto &s = mSprites[i];
     s->vx = ANGULAR_VELOCITY * 48 * SIN(mAngle + 2 * M_PI * TFloat(i) / SPRITE_COUNT);
     s->vy = ANGULAR_VELOCITY * 24 * COS(mAngle + 2 * M_PI * TFloat(i) / SPRITE_COUNT);
+    s->w = 16;
+    s->h = 8;
+    s->cx = 0;
+    s->cy = 4;
   }
 }
 
@@ -46,11 +50,16 @@ TBool GFinalBossShieldProcess::RunAfter() {
         s->Explode();
       }
       if (s->AnimDone()) {
-        s->ClearFlags(SFLAG_RENDER);
+        s->ClearFlags(SFLAG_RENDER | SFLAG_CHECK);
       }
     } else {
       s->x = mCenter.x + 48 * SIN(mAngle + 2 * M_PI * TFloat(i) / SPRITE_COUNT) - 16;
       s->y = mCenter.y + 24 * COS(mAngle + 2 * M_PI * TFloat(i) / SPRITE_COUNT) + 4;
+      // expand collision box to direct knockback away from boss
+      s->w = ABS(mCenter.x - (s->x + 16)) + 16;
+      s->cx = -s->w + 16 + (mCenter.x - ((TInt)s->x + 16)) / 2;
+      s->h = ABS(mCenter.y - (s->y + 4)) + 8;
+      s->cy = MAX(4, mCenter.y - (s->y));
     }
     s->ClearCType(STYPE_PLAYER);
   }
