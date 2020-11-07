120e350cc8289063a95f7199cf6fc80af2033040
Tom Collins <thomas.collins@moduscreate.com>
2020-11-07 16:52:08 -0500

    fixed profile file extension

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -21,4 +21,4 @@ security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keycha
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "./scripts/profile/$PROFILE_NAME.provisionprofile" ~/Library/MobileDevice/Provisioning\ Profiles/
