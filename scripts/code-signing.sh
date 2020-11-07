ea2103f91b341bf977eed8214fd0cd9cc035a27a
Tom Collins <thomas.collins@moduscreate.com>
2020-11-07 14:09:08 -0500

    added codesigning command and verification

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -4,4 +4,12 @@ OS="$(uname)"
 
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
+
+  codesign -f --timestamp --options runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+
+  echo "Verifying signature..."
+
+  codesign -v --strict --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+
+  spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
