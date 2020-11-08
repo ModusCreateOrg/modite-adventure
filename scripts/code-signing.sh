dd841e2a3e90ff9aadf1f1be2c207bd4946f311a
Tom Collins <thomas.collins@moduscreate.com>
2020-11-07 19:37:00 -0500

    cert password now works, altering codesign command to use path variable, removing strict flag from verification

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -2,15 +2,16 @@
 
 OS="$(uname)"
 DEVELOPER_NAME="Developer ID Application: Modus Create, Inc. (287TS9B2H2)"
+PATH="/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app"
 
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
 
-  codesign -f -s $DEVELOPER_NAME --timestamp --options runtime --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+  codesign -f -s $DEVELOPER_NAME --timestamp --options runtime --keychain /Users/travis/Library/Keychains/ios-build.keychain $PATH
 
   echo "Verifying signature..."
 
-  codesign -v --strict --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+  codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
   spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
