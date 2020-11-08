83e947950319d162e153502888bb4329cab4a442
TCModus <63469617+TCModus@users.noreply.github.com>
2020-11-08 11:00:39 -0500

    Feature/code signing (#697)
    * cleaned up travis.yml so it passes validation
    
    * removed xcode line in cmakelist, added separate notarize.sh script to handle notarization and code signing which is just echoing a line right now, general cleanup
    
    * upgrade xcode to version 12
    
    * re-ordered travis.yml file for readability
    
    * staged separate script for code signing vs notarization
    
    * forgot closing quotation mark in travis.yml
    
    * added codesigning command and verification
    
    * added certs and profile and scripts to add certs to new keychain
    
    * fixed profile file extension
    
    * using variable for developer name on signing instead of string literal
    
    * specified keychain to use in codesign command
    
    * exported correct password for keychain
    
    * password still not right, trying new combo
    
    * last attempt at password before it needs to be reset
    
    * still correcting password confusion
    
    * replaced certs and using fresh password
    
    * cert password now works, altering codesign command to use path variable, removing strict flag from verification
    
    * removing timestamp and options runtime flags from codesign command
    
    * codesign command not working
    
    * removed path variable as it may have been interfering with codesign command
    
    * removed ID variable and input string literal for codesign command
    
    * adding deep flag to codesign command
    
    * added travis_wait keyword to code signing step as it takes longer than 10 minutes to complete with no output
    
    * fixing travis_wait
    
    * removing travis_wait and adding verbosity to codesign command
    
    * adding timestamp flag
    
    * setting 90 minute travis_wait timer to see if codesigning will finish successfully
    
    * adding security command to set key partition for apple tools
    
    * added options runtime flag to code sign for notarization

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -4,4 +4,12 @@ OS="$(uname)"
 
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
+
+  codesign --force --deep --verbose --timestamp --options=runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+
+  echo "Verifying signature..."
+
+  codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+
+  spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
