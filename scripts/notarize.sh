2829a3e8d446371058433677c7ac20fa87ed4be9
Tom Collins <thomas.collins@moduscreate.com>
2020-11-08 17:19:40 -0500

    added notarize command

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 OS="$(uname)"
+USER=""
+PASS=""
 
 if [[ "$OS" == "Darwin" ]]; then
   echo "notarizing..."
+
+  xcrun altool --notarize-app --primary-bundle-id "com.moduscreate.modite-adventure" -u $USER -p $PASS --asc-provider 287TS9B2H2 --file /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
