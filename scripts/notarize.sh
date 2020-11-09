26ae0fb349322c22fb5f712bc9ff925b20f8c8e0
TCModus <63469617+TCModus@users.noreply.github.com>
2020-11-09 13:41:59 -0500

    Feature/notarization (#701)
    * removing draft tag in travis.yml to fix releasing
    
    * re-added draft tag and added pre tagging step to update local tags
    
    * added notarize command
    
    * deploying releases on all branches while diagnosing release issue
    
    * statically setting username for notarization
    
    * added comment
    
    * attempting to clear stash
    
    * fixing clear command
    
    * I think travis is trying to install an update in the middle of deployment
    
    * changing release version and added app pw for notarization as an encrypted variable in travis
    
    * seting overwrite to false and sticking with original release tag
    
    * setting cleanup tag in travis.yml
    
    * skip cleanup set to true
    
    * adding overwrite true with skip cleanup true
    
    * setting skip cleanup flag with string for true
    
    * removing depreciated skip-cleanup flag and draft flag, fixing travis variable reference in notarize.sh
    
    * travis docs say skip-cleanup flag is depreciated however release breaks without it
    
    * fixed notarize script to point to zip file instead of binary folder
    
    * fixing provider shortname in notarization
    
    * setting deploy to only release on master branch again

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 OS="$(uname)"
+USER="thomas.collins@moduscreate.com"
 
 if [[ "$OS" == "Darwin" ]]; then
   echo "notarizing..."
+
+  xcrun altool --notarize-app --primary-bundle-id "com.moduscreate.modite-adventure" -u $USER -p ${APP_PASS} --asc-provider ModusCreateInc --file /Users/travis/build/ModusCreateOrg/modite-adventure/build/modite-mac.zip
 fi
