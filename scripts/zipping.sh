571bf4412b013f105df404262041339b34c3f008
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 14:44:04 -0500

    adding exeutable permissions to binary before zipping and notarizing

--- scripts/zipping.sh
+++ scripts/zipping.sh
@@ -3,6 +3,7 @@
 OS="$(uname)"
 
 if [ "$OS" == "Darwin" ]; then
+  chmod +x /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/Modite
   cd /Users/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite-mac.zip Modite.app/ && cd -
 elif [ "$OS" == "Linux" ]; then
   cd /home/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks modite-linux.zip Modite && cd -
