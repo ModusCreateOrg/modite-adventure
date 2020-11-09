88eea61f67b78ec43f208590472ef54142feaf05
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 18:17:51 -0500

    removing codesigning and notarization and will fix and test in bugfix branch

--- scripts/staple.sh
+++ scripts/staple.sh
@@ -6,4 +6,6 @@ if [[ "$OS" == "Darwin" ]]; then
   xcrun stapler staple /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
   rm /Users/travis/build/ModusCreateOrg/modite-adventure/build/modite-mac.zip
+
+  cd /Users/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite-mac.zip Modite.app/ && cd -
 fi
