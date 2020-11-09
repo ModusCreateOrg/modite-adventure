407e5f76a1d49b31c40b5a114d4741d705ee6008
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 17:20:08 -0500

    stapling notarization ticket to app

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -7,4 +7,6 @@ if [[ "$OS" == "Darwin" ]]; then
   echo "notarizing..."
 
   xcrun altool --notarize-app --primary-bundle-id "com.moduscreate.modite-adventure" -u $USER -p ${APP_PASS} --asc-provider ModusCreateInc --file /Users/travis/build/ModusCreateOrg/modite-adventure/build/modite-mac.zip
+
+  xcrun stapler staple "Modite.app"
 fi
