3afa2c81156907eb0fd4d3b28a404521ba714416
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 12:47:07 -0500

    removing depreciated skip-cleanup flag and draft flag, fixing travis variable reference in notarize.sh

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -6,5 +6,5 @@ USER="thomas.collins@moduscreate.com"
 if [[ "$OS" == "Darwin" ]]; then
   echo "notarizing..."
 
-  xcrun altool --notarize-app --primary-bundle-id "com.moduscreate.modite-adventure" -u $USER -p $APP_PASS --asc-provider 287TS9B2H2 --file /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+  xcrun altool --notarize-app --primary-bundle-id "com.moduscreate.modite-adventure" -u $USER -p ${APP_PASS} --asc-provider 287TS9B2H2 --file /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
