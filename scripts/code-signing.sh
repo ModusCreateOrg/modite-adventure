755312d00f4f3c27bbd9a7d8ecaa67f566c6e397
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 17:09:13 -0500

    attempting to codesign sdl2 libs individually in addition to codesigning the app with the deep option

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -5,6 +5,13 @@ OS="$(uname)"
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
 
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libjpeg.dylib
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libpng.dylib
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2.dylib
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libtiff.dylib
+  codesign -f -s - /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libwebp.dylib
+
   codesign --force --deep --verbose --timestamp --options=runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
   echo "Verifying signature..."
