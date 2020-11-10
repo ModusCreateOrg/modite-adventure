68ceb23139b0f7db4c0c1477d2ec8d1517e4ceac
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 22:44:22 -0500

    removed force option on sdl2 disk image file for codesigning

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -5,13 +5,15 @@ OS="$(uname)"
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
 
-  codesign --force --verbose --timestamp --options=runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
+  codesign --verbose --timestamp --options=runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
   codesign --force --verbose --timestamp --options=runtime -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
   echo "Verifying signature..."
 
   codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
   codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+
+  echo "spctl Verification..."
 
   spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
   spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
