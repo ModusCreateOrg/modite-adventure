fc48e418f5d002e6cff52d8a05b42fffbc160f5f
Tom Collins <thomas.collins@moduscreate.com>
2020-11-10 13:47:32 -0500

    removing chmods from zipping script

--- scripts/zipping.sh
+++ scripts/zipping.sh
@@ -3,15 +3,6 @@
 OS="$(uname)"
 
 if [ "$OS" == "Darwin" ]; then
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libwebp.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libtiff.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libpng.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libjpeg.dylib"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/Modite"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/Modite.bin"
-  chmod +x "/Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app"
   cd /Users/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks -r modite-mac.zip Modite.app/ && cd -
 elif [ "$OS" == "Linux" ]; then
   cd /home/travis/build/ModusCreateOrg/modite-adventure/build && zip --symlinks modite-linux.zip Modite && cd -
