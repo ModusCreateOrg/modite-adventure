50da00005d68e2067b30f83f29d62673cad9dc52
Tom Collins <thomas.collins@moduscreate.com>
2020-11-09 21:52:12 -0500

    found sdl2 image that needs to be separately codesigned

--- scripts/code-signing.sh
+++ scripts/code-signing.sh
@@ -5,11 +5,14 @@ OS="$(uname)"
 if [[ "$OS" == "Darwin" ]]; then
   echo "Code signing..."
 
-  codesign --force --verbose --timestamp --options runtime -i com.moduscreate.modite-adventure.Modite -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
+  codesign --force --verbose --timestamp --options=runtime -i com.moduscreate.modite-adventure.Modite -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
+  codesign --force --verbose --timestamp --options=runtime -i com.moduscreate.modite-adventure.Modite -s "Developer ID Application: Modus Create, Inc. (287TS9B2H2)" --keychain /Users/travis/Library/Keychains/ios-build.keychain /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
   echo "Verifying signature..."
 
+  codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
   codesign -v --deep --verbose=2 /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 
+  spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app/Contents/MacOS/libs/libSDL2_image.dylib
   spctl --assess -vv /Users/travis/build/ModusCreateOrg/modite-adventure/build/Modite.app
 fi
