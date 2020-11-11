0e513b318ddb4461118ab3913dad2acf7406bc9d
Jay Garcia <jesusdgarcia@gmail.com>
2020-11-11 12:44:04 -0500

    Level tweaks (#702)
    * +2 levels for OW3 DGN4 (Midboss before the wizard)
    
    * +2 levels for OW6 DGN4 (Midboss before the wizard)
    
    * +2 levels for OW7 DGN4 (Midboss before the wizard)

--- src/GameState/DungeonDefs.h
+++ src/GameState/DungeonDefs.h
@@ -268,8 +268,8 @@ struct TDungeonInfo gDungeonDefs[] = {
         OW3_DGN4_3072_OW3_DGN4_L2_MAP, // 2
         OW3_DGN4_3072_OW3_DGN4_L3_MAP, // 3
         OW3_DGN4_3072_OW3_DGN4_L4_MAP, // 4
-        -1, //OW3_DGN4_3072_OW3_DGN4_L5_MAP, // 5
-        -1, //OW3_DGN4_3072_OW3_DGN4_L6_MAP, // 6
+        OW3_DGN4_3072_OW3_DGN4_L5_MAP, // 5
+        OW3_DGN4_3072_OW3_DGN4_L6_MAP, // 6
         -1, //OW3_DGN4_3072_OW3_DGN4_L7_MAP, // 7
         -1, //OW3_DGN4_3072_OW3_DGN4_L8_MAP, // 8
         -1, // OW3_DGN4_3072_OW3_DGN4_L9_MAP, // 9
@@ -520,8 +520,8 @@ struct TDungeonInfo gDungeonDefs[] = {
         OW6_DGN4_6144_OW6_DGN4_L2_MAP, // 2
         OW6_DGN4_6144_OW6_DGN4_L3_MAP, // 3
         OW6_DGN4_6144_OW6_DGN4_L4_MAP, // 4
-        -1, //OW6_DGN4_6144_OW6_DGN4_L5_MAP, // 5
-        -1, //OW6_DGN4_6144_OW6_DGN4_L6_MAP, // 6
+        OW6_DGN4_6144_OW6_DGN4_L5_MAP, // 5
+        OW6_DGN4_6144_OW6_DGN4_L6_MAP, // 6
         -1, //OW6_DGN4_6144_OW6_DGN4_L7_MAP, // 7
         -1, //OW6_DGN4_6144_OW6_DGN4_L8_MAP, // 8
         -1, //OW6_DGN4_6144_OW6_DGN4_L9_MAP, // 9
@@ -605,8 +605,8 @@ struct TDungeonInfo gDungeonDefs[] = {
         OW7_DGN4_7168_OW7_DGN4_L3_MAP, // 3
         OW7_DGN4_7168_OW7_DGN4_L4_MAP, // 4
         OW7_DGN4_7168_OW7_DGN4_L5_MAP, // 5
-        -1, //OW7_DGN4_7168_OW7_DGN4_L6_MAP, // 6
-        -1, //OW7_DGN4_7168_OW7_DGN4_L7_MAP, // 7
+        OW7_DGN4_7168_OW7_DGN4_L6_MAP, // 6
+        OW7_DGN4_7168_OW7_DGN4_L7_MAP, // 7
         -1, //OW7_DGN4_7168_OW7_DGN4_L8_MAP, // 8
         -1, //OW7_DGN4_7168_OW7_DGN4_L9_MAP, // 9
         -1 // EMPTY -- no level 10 evar!!
