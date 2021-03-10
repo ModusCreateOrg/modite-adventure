2aa1e28acf0100ebf341aa13b9b0f42c62aace32
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-10 15:43:58 -0500

    Testing for network display

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,28 @@ BGameEngine *gGameEngine;
 TOptions *gOptions;
 GGame *gGame;
 
+
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+#include "NetworkDisplay/NetworkDisplay.h"
+#endif
+
 // app_main
-extern "C" void app_main() {
+extern "C" void app_main(const char *file[]) {
+
     gGame = new GGame();
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+    NetworkDisplay *display = (NetworkDisplay*)&gDisplay;
+    const char *fileName = "/home/jgarcia/modite-rgb.ini";
+    display->ConfigureFromIniFile(fileName);
+#endif
     gGame->Run();
+
     delete gGame;
 }
 
 
-int main() {
-    app_main();
+int main(int argc, const char *file[]) {
+    app_main(file);
     return 0;
 }
 
