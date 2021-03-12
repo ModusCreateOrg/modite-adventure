423a6961f11b598e6d558e420aafdd5518599da9
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 15:32:32 -0500

    Added path checking

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,14 @@ extern "C" void app_main(char *fileName) {
 
 
 int main(int argc, char *argv[]) {
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+  if (argc < 2) {
+    printf("Usage: ./%s <path.to.ini.file>\n", argv[1]);
+    return 1;
+  }
+#endif
 
-    app_main(argv[0]);
+    app_main(argv[1]);
     return 0;
 }
 
